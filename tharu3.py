69f0ef1d56c3ae3f99f2ba3f9e05780be07118e1
Pasindu Nishshanka <87374999+Pasindu-N-01@users.noreply.github.com>
2022-12-08 11:55:37 +0530

    Add files via upload

---
+++ tharu3.py
@@ -0,0 +1,9 @@
+
+import array as arr
+a = arr.array('i', [2, 4, 6, 8,10])
+
+print("First element:", a[0])
+print("Second element:", a[1])
+print("Third element:", a[2])
+print("Forth element:", a[3])
+print("Fifth element:", a[4])
